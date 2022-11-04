ceeb4075e20bb2b6efd40a38a34e943698d2afca
Piyush Arora <arora.8@iitj.ac.in>
2022-11-05 03:04:44 +0530

    text2speech

--- module/utils.py
+++ module/utils.py
@@ -2,19 +2,24 @@ import sys
 import os
 import webbrowser
 import speech_recognition as sr
+from gtts import gTTS
+from io import BytesIO
+import pygame
+import time
 
 def takeCommandVoice():
     r = sr.Recognizer()
     query = "None"
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.dynamic_energy_threshold = True
         r.pause_threshold = 1
         audio = r.listen(source)
     try:
         query = r.recognize_google(audio, language='en-in')
-        print(f"[Query Interpreted by me: {query}]\n")
+        print_(f"[Query Interpreted by me: {query}]")
     except Exception as e:
-        print(e)
-        print("[I am really sorry, Say that again please...]")
+        print_("[I am really sorry, Say that again please...]")
     return query
 
 def takeCommand():
@@ -27,16 +32,34 @@ def command_voice_text(inp):
     elif inp == 'voice':
         return takeCommandVoice()
 
+def get_audio(text, lang = 'en'):
+    mp3_fo = BytesIO()
+    tts = gTTS(text=text, lang=lang)
+    tts.write_to_fp(mp3_fo)
+    return mp3_fo
+
+def print_(text):
+    print(text)
+    pygame.init()
+    pygame.mixer.init()
+    music = get_audio(text)
+
+    pygame.mixer.music.load(music, 'mp3')
+
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+    
 
 def print_introduction():
-    print("Welcome to the all new CMD controlled by your voice!")
-    print("Type 'help' to see all the commands available.")
+    print_("Welcome to the all new CMD controlled by your voice!")
 
 def print_cwd(ch):
     if ch == 'text':
         print(f"{os.getcwd()}$ ", end="")
     elif ch == 'voice':
-        print(os.getcwd()+': Try saying a command')
+        print(os.getcwd(), end = '')
+        print_(': Try saying a command')
     else:
         print("Invalid choice")
 
@@ -81,17 +104,17 @@ def cp(src, dst):
 def cat(path):
     try:
         with open(path, 'r') as file:
-            print(file.read())
+            print_(file.read())
     except OSError as error:
         print_error(error)
 
 def echo(text):
-    print(text)
+    print_(text)
 
 def ls(path):
     try:
         for file in os.listdir(path):
-            print(file)
+            print_(file)
     except OSError as error:
         print_error(error)
 
@@ -102,53 +125,58 @@ def cd(path):
         print_error(error)
 
 def pwd():
-    print(os.getcwd())
+    print_(os.getcwd())
 
 def exit():
     sys.exit()
 
 def print_help():
-    print("help - print this message")
-    print("clear - clear the screen")
-    print("exit - exit the program")
-    print("cd - change directory")
-    print("ls - list files in directory")
-    print("pwd - print working directory")
-    print("mkdir - make directory")
-    print("rmdir - remove directory")
-    print("rm - remove file")
-    print("mv - move file")
-    print("cp - copy file")
-    print("cat - print file contents")
-    print("echo - print text")
+    print_("help - print this message")
+    print_("clear - clear the screen")
+    print_("exit - exit the program")
+    print_("cd - change directory")
+    print_("ls - list files in directory")
+    print_("pwd - print working directory")
+    print_("mkdir - make directory")
+    print_("rmdir - remove directory")
+    print_("rm - remove file")
+    print_("mv - move file")
+    print_("cp - copy file")
+    print_("cat - print file contents")
+    print_("echo - print text")
+    print_("google - search on google")
+    print_("youtube - play music on youtube")
+    print_("snake - play snake and ladder")
+    print_("chess - play chess")
+
 
 def try_google(command):
     try:
-        print("Searching on Google...")
+        print_("Searching on Google...")
         webbrowser.open("https://www.google.com/search?q=" + command)
     except Exception as e:
-        print(e)
+        print_(e)
 
 def play_music_on_youtube(command):
     try:
-        print("Playing on YouTube...")
+        print_("Playing on YouTube...")
         webbrowser.open("https://www.youtube.com/results?search_query=" + command)
     except Exception as e:
-        print(e)
+        print_(e)
 
 def snake_and_ladder():
     try:
-        print("Opening Snake and Ladder...")
+        print_("Opening Snake and Ladder...")
         webbrowser.open("https://toytheater.com/snakes-and-ladders/")
     except Exception as e:
-        print(e)
+        print_(e)
 
 def chess():
     try:
-        print("Opening Chess...")
+        print_("Opening Chess...")
         webbrowser.open("https://www.chess.com/play/computer")
     except Exception as e:
-        print(e)
+        print_(e)
 
 def main_cmd(input_choice):
   while True:
@@ -162,7 +190,7 @@ def main_cmd(input_choice):
     elif command == "clear":
         clear()
     elif command == "exit":
-        print("[Bye!][Thanks for using me]")
+        print_("[Bye!][Thanks for using me]")
         exit()
     elif command == "pwd":
         pwd()
@@ -195,7 +223,7 @@ def main_cmd(input_choice):
     elif command.startswith("chess"):
         chess()
     else:
-        print("[Unknown command]")
+        print_("[Unknown command]")
         opt = input("Wanna google this? (y/n): ")
         if opt == 'y':
             try_google(command)
